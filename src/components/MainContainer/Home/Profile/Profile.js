152e6be5bdcf65147ae293b797ee03940ede6eb9
Vikas Saini <vikassaini883@gmail.com>
2023-05-09 21:33:35 -0400

    Updated Profilt Buttons

--- src/components/MainContainer/Home/Profile/Profile.js
+++ src/components/MainContainer/Home/Profile/Profile.js
@@ -9,23 +9,30 @@ function Profile(props) {
 
   const style = {
     profileInfoStyle: {
-      color: darkTheme ? "#dddddd" : "#353535",
+      color: darkTheme ? "#f8f8f8" : "#353535",
     },
     iconStyle: {
-      color: darkTheme ? "#dddddd" : "#353535",
+      color: darkTheme ? "#f8f8f8" : "#353535",
     },
     lineStyle: {
-      boderColor: darkTheme ? "#f9f9f9" : "#212529",
+      boderColor: darkTheme ? "#f8f8f8" : "#212529",
     },
     profilePicStyle: {
       boxShadow: darkTheme
         ? "0 1px 0px 0px rgb(249, 248, 248)"
         : "0 1px 0px 0px rgb(29, 27, 27)",
     },
-    buttonStyle: {
+    ResumeButtonStyle: {
       boxShadow: darkTheme
         ? "2px 5px 5px rgb(22, 21, 21)"
-        : "2px 5px 5px rgb(156, 154, 154)",
+        : "2px 5px 5px rgb(180, 180, 180)",
+      color: darkTheme ? "" : "",
+    },
+    downloadStyle: {
+      color: darkTheme ? "#353535" : "#f9f9f9",
+    },
+    btnTheme: {
+      color: darkTheme ? "light" : "dark",
     },
   };
 
@@ -37,10 +44,7 @@ function Profile(props) {
         </div>
         <div className="profile-info" style={style.profileInfoStyle}>
           <h1 className="profile-tagline">
-            Hi! I am{" "}
-            <b className="funky-name" style={style.profileInfoStyle}>
-              Vikas Saini
-            </b>
+            Hi! I am <b className="funky-name">Vikas Saini</b>
           </h1>
           <h1 className="profile-tagline">
             I am a{" "}
@@ -64,39 +68,21 @@ function Profile(props) {
             </a>
           </div>
           <div className="buttons">
-            <a
+            <button
               type="button"
               href={require("../../../../Assets/Vikas_Saini_CV.pdf")}
-              className="rainbow-hover"
-              style={style.buttonStyle}
+              className={"btn btn-lg btn-" + style.btnTheme.color}
+              style={style.ResumeButtonStyle}
             >
-              <FontAwesomeIcon icon={faDownload} style={{ color: "#cfef00" }} />
+              <FontAwesomeIcon icon={faDownload} style={style.downloadStyle} />
               Resume
-            </a>
+            </button>
             <button
-              className="btn-about-me"
+              type="button"
+              className={"btn btn-lg btn-outline-" + style.btnTheme.color}
               onClick={() => props.onComponentClick(props.contactRef)}
             >
-              <span>Contact Me</span>
-              <svg
-                width="34"
-                height="34"
-                viewBox="0 0 74 74"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <circle
-                  cx="37"
-                  cy="37"
-                  r="35.5"
-                  stroke="black"
-                  strokeWidth="3"
-                ></circle>
-                <path
-                  d="M25 35.5C24.1716 35.5 23.5 36.1716 23.5 37C23.5 37.8284 24.1716 38.5 25 38.5V35.5ZM49.0607 38.0607C49.6464 37.4749 49.6464 36.5251 49.0607 35.9393L39.5147 26.3934C38.9289 25.8076 37.9792 25.8076 37.3934 26.3934C36.8076 26.9792 36.8076 27.9289 37.3934 28.5147L45.8787 37L37.3934 45.4853C36.8076 46.0711 36.8076 47.0208 37.3934 47.6066C37.9792 48.1924 38.9289 48.1924 39.5147 47.6066L49.0607 38.0607ZM25 38.5L48 38.5V35.5L25 35.5V38.5Z"
-                  fill="black"
-                ></path>
-              </svg>
+              Contact Me
             </button>
           </div>
         </div>
